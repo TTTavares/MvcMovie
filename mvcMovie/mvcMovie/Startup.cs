7c396c4f348259eb822ae79386750e4ff5df8077
TTTavares <43744305+TTTavares@users.noreply.github.com>
2018-10-13 21:52:50 +0100

    Adicinado Movie Model

--- mvcMovie/mvcMovie/Startup.cs
+++ mvcMovie/mvcMovie/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using mvcMovie.Models;
 
 namespace mvcMovie
 {
@@ -33,6 +35,9 @@ namespace mvcMovie
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<mvcMovieContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("mvcMovieContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
